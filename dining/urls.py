88ca8d297f468aad729d9554c4a4cd9b8f9d41b4
losanki <solankiravishankar@gmail.com>
2020-08-23 22:59:35 +0530

    Implement dashboard

--- dining/urls.py
+++ dining/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.conf.urls import url, include
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='dashboard.html'), name='dashboard'),
     path('menu/', views.MenuItemsView.as_view(), name='menus'),
     path('menu/<int:pk>/', views.MenuDetailView.as_view(), name='menu_detail'),
     path('meal/<int:pk>/', views.MealDetailView.as_view(), name='meal_detail'),

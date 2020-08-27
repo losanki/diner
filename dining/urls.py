ba7ff2acc11c8fe220d7e62653225af7ada9eb75
Ravi Solanki <solankiravishankar@gmail.com>
2020-08-27 13:13:30 +0530

    New week url and remove order stuff

--- dining/urls.py
+++ dining/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from django.conf.urls import url, include
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ArchiveIndexView
+from dining.models import Menu
 
 from . import views
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='dashboard.html'), name='dashboard'),
+    path('', views.DashboardView.as_view(), name='dashboard'),
     path('menu/', views.MenuItemsView.as_view(), name='menus'),
+    path('<int:year>/week/<int:week>/', views.MenuWeekView.as_view(), name='menus-week'),
+    path('menu-latest/', ArchiveIndexView.as_view(model=Menu, date_field='date'), name='menus-latest'),
     path('menu/<int:pk>/', views.MenuDetailView.as_view(), name='menu_detail'),
     path('meal/<int:pk>/', views.MealDetailView.as_view(), name='meal_detail'),
     path('meal/<int:pk>/order', views.OrderCreateView.as_view(), name='order'),
--- dining/views.py
+++ dining/views.py
@@ -1,19 +1,61 @@
-from django.views.generic import ListView, DetailView, View
+from django.views.generic import ListView, DetailView, View, WeekArchiveView
 from .models import Menu, Order, Meal
 import datetime
 from django.shortcuts import HttpResponseRedirect, render
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils.dates import WEEKDAYS
+
+
+def get_current_week():
+    time = datetime.datetime.today().isocalendar()
+    year = time[0]
+    week = time[1] - 1
+    return year, week
+
+
+class DashboardView(View):
+
+    def get(self, request):
+        year, week = get_current_week()
+        context = {'year': year, 'week': week}
+        return render(request, 'dashboard.html', context=context)
 
 
 class MenuItemsView(ListView):
     context_object_name = 'week_list'
     model = Menu
-    dow = datetime.date.weekday(datetime.datetime.now()) # get current day of week e.g, 6
+    dow = datetime.date.weekday(datetime.date.today())  # get current day of week e.g, 6
     mon = datetime.datetime.now() - datetime.timedelta(days=dow)
     weekend = mon + datetime.timedelta(days=5)
     queryset = Menu.objects.filter(date__gte=mon)
     queryset = queryset.filter(date__lte=weekend)
     template_name = 'week.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        weekday_names = []
+        meal_names = ['Breakfast', 'Lunch', 'Snack']
+        for i in range(6):
+            weekday_names.append(WEEKDAYS[i])
+        context['weekday_names'] = weekday_names
+        context['meal_names'] = meal_names
+        return context
+
+
+class MenuWeekView(WeekArchiveView):
+    queryset = Menu.objects.all()
+    date_field = 'date'
+    week_format = '%W'
+    allow_future = True
+    ordering = 'date'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        weekday_names = []
+        for i in range(6):
+            weekday_names.append(WEEKDAYS[i])
+        context['weekday_names'] = weekday_names
+        return context
 
 
 class MenuDetailView(DetailView):
@@ -23,7 +65,13 @@ class MenuDetailView(DetailView):
 
 class MealDetailView(DetailView):
     model = Meal
-    template_name = 'meal_detail.html'
+
+    def get_context_data(self, **kwargs):
+        year, week = get_current_week()
+        context = super().get_context_data(**kwargs)
+        context['year'] = year
+        context['week'] = week
+        return context
 
 
 class OrderCreateView(LoginRequiredMixin, View):
